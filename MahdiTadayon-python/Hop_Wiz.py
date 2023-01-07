b1da13f1c5a7c41d1ec462df1d5fa68e95c36d25
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-07 23:04:59 +0330

    Add some spaces

--- MahdiTadayon-python/Hop_Wiz.py
+++ MahdiTadayon-python/Hop_Wiz.py
@@ -4,7 +4,7 @@
 # multiples of 3 and 7 --> HopWiz
 
 # solution 1 
-for number  in range(1,100):print("HopViz") if not number % 21  else print("Hop") if not number % 3  else print("Viz") if not number % 7  else print(number)
+for number  in range(1, 100):print("HopViz") if not number % 21  else print("Hop") if not number % 3  else print("Viz") if not number % 7  else print(number)
 # solution 2 ---> pythonic 
-for number  in range(1,100):print("HopViz") if number in range(0,100,21) else print("Hop") if number in range(0,100,3) else print("Viz") if number in range(0,100,7) else print(number)
+for number  in range(1, 100):print("HopViz") if number in range(0,100,21) else print("Hop") if number in range(0,100,3) else print("Viz") if number in range(0,100,7) else print(number)
 
