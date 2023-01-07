b13d3712f6b1a5526102c4b7e103152b3bd3ea74
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-07 23:11:56 +0330

    Add some spaces for better readability

--- MahdiTadayon-python/prime_numbers.py
+++ MahdiTadayon-python/prime_numbers.py
@@ -1,7 +1,7 @@
 # prime numbers(1 - 1000) ---> for and else 
 # solution 1 
-for number in range(2,1000):
-    for divisor in range(2,number):
+for number in range(2, 1000):
+    for divisor in range(2, number):
         if (number  % divisor == 0):  
             break 
     else:
@@ -9,9 +9,9 @@ for number in range(2,1000):
 
 
 # solution 2
-for number in range(2,1000):
-    for divisor in range(2,number):
-        if number in range(0,number+1,divisor):
+for number in range(2, 1000):
+    for divisor in range(2, number):
+        if number in range(0, number+1, divisor):
             break 
     else:
         print(number) 
